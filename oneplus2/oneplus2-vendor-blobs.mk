bd1c66de8eb6e324b41d935ea342c2bfcf8a64eb
Willi Ye <williye97@gmail.com>
2015-09-18 21:12:04 +0200

    oneplus2: Add fingerprint and lights

--- oneplus2/oneplus2-vendor-blobs.mk
+++ oneplus2/oneplus2-vendor-blobs.mk
@@ -450,21 +450,22 @@ PRODUCT_COPY_FILES += \
         vendor/oneplus/oneplus2/proprietary/vendor/lib64/libril-qcril-hook-oem.so:/system/vendor/lib64/libril-qcril-hook-oem.so \
         vendor/oneplus/oneplus2/proprietary/vendor/lib64/libxml.so:/system/vendor/lib64/libxml.so \
         vendor/oneplus/oneplus2/proprietary/bin/sensors.qcom:/system/bin/sensors.qcom \
-        vendor/oneplus/oneplus2/proprietary/etc/fpc1021w160.bin:/system/etc/fpc1021w160.bin \
         vendor/oneplus/oneplus2/proprietary/etc/firmware/tp/14049/14049_FW_S1302.img:/system/etc/firmware/tp/14049/14049_FW_S1302.img \
         vendor/oneplus/oneplus2/proprietary/etc/firmware/tp/14049/14049_FW_S3320_jdi.img:/system/etc/firmware/tp/14049/14049_FW_S3320_jdi.img \
-        vendor/oneplus/oneplus2/proprietary/lib/hw/wbc_hal.default.so:/system/lib/hw/wbc_hal.default.so \
         vendor/oneplus/oneplus2/proprietary/lib64/hw/wbc_hal.default.so:/system/lib64/hw/wbc_hal.default.so \
-        vendor/oneplus/oneplus2/proprietary/vendor/lib/hw/sensors.msm8994.so:/system/vendor/lib/hw/sensors.msm8994.so \
-        vendor/oneplus/oneplus2/proprietary/vendor/lib64/hw/sensors.msm8994.so:/system/vendor/lib64/hw/sensors.msm8994.so \
-        vendor/oneplus/oneplus2/proprietary/vendor/lib/hw/sensors.oem.so:/system/vendor/lib/hw/sensors.oem.so \
-        vendor/oneplus/oneplus2/proprietary/vendor/lib64/hw/sensors.oem.so:/system/vendor/lib64/hw/sensors.oem.so \
+        vendor/oneplus/oneplus2/proprietary/lib/hw/wbc_hal.default.so:/system/lib/hw/wbc_hal.default.so \
         vendor/oneplus/oneplus2/proprietary/vendor/lib64/hw/activity_recognition.msm8994.so:/system/vendor/lib64/hw/activity_recognition.msm8994.so \
         vendor/oneplus/oneplus2/proprietary/vendor/lib64/libsensor1.so:/system/vendor/lib64/libsensor1.so \
         vendor/oneplus/oneplus2/proprietary/vendor/lib64/libsensor_reg.so:/system/vendor/lib64/libsensor_reg.so \
+        vendor/oneplus/oneplus2/proprietary/vendor/lib64/hw/sensors.msm8994.so:/system/vendor/lib64/hw/sensors.msm8994.so \
+        vendor/oneplus/oneplus2/proprietary/vendor/lib64/hw/sensors.oem.so:/system/vendor/lib64/hw/sensors.oem.so \
         vendor/oneplus/oneplus2/proprietary/vendor/lib/hw/activity_recognition.msm8994.so:/system/vendor/lib/hw/activity_recognition.msm8994.so \
         vendor/oneplus/oneplus2/proprietary/vendor/lib/libsensor1.so:/system/vendor/lib/libsensor1.so \
         vendor/oneplus/oneplus2/proprietary/vendor/lib/libsensor_reg.so:/system/vendor/lib/libsensor_reg.so \
+        vendor/oneplus/oneplus2/proprietary/vendor/lib/hw/sensors.msm8994.so:/system/vendor/lib/hw/sensors.msm8994.so \
+        vendor/oneplus/oneplus2/proprietary/vendor/lib/hw/sensors.oem.so:/system/vendor/lib/hw/sensors.oem.so \
+        vendor/oneplus/oneplus2/proprietary/lib64/hw/lights.msm8994.so:/system/lib64/hw/lights.msm8994.so \
+        vendor/oneplus/oneplus2/proprietary/lib/hw/lights.msm8994.so:/system/lib/hw/lights.msm8994.so \
         vendor/oneplus/oneplus2/proprietary/bin/qseecomd:/system/bin/qseecomd \
         vendor/oneplus/oneplus2/proprietary/vendor/lib64/libdrmfs.so:/system/vendor/lib64/libdrmfs.so \
         vendor/oneplus/oneplus2/proprietary/vendor/lib64/libdrmtime.so:/system/vendor/lib64/libdrmtime.so \
@@ -508,6 +509,23 @@ PRODUCT_COPY_FILES += \
         vendor/oneplus/oneplus2/proprietary/vendor/lib/libmm-disp-apis.so:/system/vendor/lib/libmm-disp-apis.so \
         vendor/oneplus/oneplus2/proprietary/vendor/lib/libmm-qdcm.so:/system/vendor/lib/libmm-qdcm.so \
         vendor/oneplus/oneplus2/proprietary/vendor/lib/libsd_sdk_display.so:/system/vendor/lib/libsd_sdk_display.so \
+        vendor/oneplus/oneplus2/proprietary/bin/qfp-daemon:/system/bin/qfp-daemon \
+        vendor/oneplus/oneplus2/proprietary/etc/fpc1021w160.bin:/system/etc/fpc1021w160.bin \
+        vendor/oneplus/oneplus2/proprietary/etc/firmware/fingerprints.b00:/system/etc/firmware/fingerprints.b00 \
+        vendor/oneplus/oneplus2/proprietary/etc/firmware/fingerprints.b03:/system/etc/firmware/fingerprints.b03 \
+        vendor/oneplus/oneplus2/proprietary/etc/firmware/fingerprints.b02:/system/etc/firmware/fingerprints.b02 \
+        vendor/oneplus/oneplus2/proprietary/etc/firmware/fingerprints.b01:/system/etc/firmware/fingerprints.b01 \
+        vendor/oneplus/oneplus2/proprietary/etc/firmware/fingerprints.mdt:/system/etc/firmware/fingerprints.mdt \
+        vendor/oneplus/oneplus2/proprietary/etc/firmware/fingerprintsecurity.b00:/system/etc/firmware/fingerprintsecurity.b00 \
+        vendor/oneplus/oneplus2/proprietary/etc/firmware/fingerprintsecurity.b01:/system/etc/firmware/fingerprintsecurity.b01 \
+        vendor/oneplus/oneplus2/proprietary/etc/firmware/fingerprintsecurity.b02:/system/etc/firmware/fingerprintsecurity.b02 \
+        vendor/oneplus/oneplus2/proprietary/etc/firmware/fingerprintsecurity.b03:/system/etc/firmware/fingerprintsecurity.b03 \
+        vendor/oneplus/oneplus2/proprietary/etc/firmware/fingerprintsecurity.mdt:/system/etc/firmware/fingerprintsecurity.mdt \
+        vendor/oneplus/oneplus2/proprietary/lib/hw/fingerprint.msm8994.so:/system/lib/hw/fingerprint.msm8994.so \
+        vendor/oneplus/oneplus2/proprietary/lib64/libqseecom_fingerprints_client.so:/system/lib64/libqseecom_fingerprints_client.so \
+        vendor/oneplus/oneplus2/proprietary/lib/libqseecom_fingerprints_client.so:/system/lib/libqseecom_fingerprints_client.so \
+        vendor/oneplus/oneplus2/proprietary/vendor/lib64/libqfp-service.so:/system/vendor/lib64/libqfp-service.so \
+        vendor/oneplus/oneplus2/proprietary/vendor/lib/libfpcalgorithm.so:/system/vendor/lib/libfpcalgorithm.so \
         vendor/oneplus/oneplus2/proprietary/lib64/libcnefeatureconfig.so:/system/lib64/libcnefeatureconfig.so \
         vendor/oneplus/oneplus2/proprietary/lib/libcnefeatureconfig.so:/system/lib/libcnefeatureconfig.so \
         vendor/oneplus/oneplus2/proprietary/vendor/lib64/libcneapiclient.so:/system/vendor/lib64/libcneapiclient.so \
